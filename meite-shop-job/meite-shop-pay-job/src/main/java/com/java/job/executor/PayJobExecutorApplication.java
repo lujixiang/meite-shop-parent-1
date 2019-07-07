deeb97791aea2b79161d5e54541b582384ac2b4a
jiangli <31346337@qq.com>
2019-07-08 00:31:12 +0800

    集成xxl-job分布式任务调度管理

---
+++ meite-shop-job/meite-shop-pay-job/src/main/java/com/java/job/executor/PayJobExecutorApplication.java
@@ -0,0 +1,16 @@
+package com.java.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *支付服务任务调度
+ */
+@SpringBootApplication
+public class PayJobExecutorApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(PayJobExecutorApplication.class, args);
+	}
+
+}
